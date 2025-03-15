e41e4ade69fa472e7854160f548d72869ccccfc1
hongsikcho <hongsikcho@DESKTOP-RNA3811>
2025-03-15 23:35:50 +0900

    초기 커밋.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace billingService.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
